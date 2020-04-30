94a6dbb227793b18bdee1448a3940f1c0b5e244e
LIU GUANG CHEN <boyislight@gmail.com>
2020-04-30 11:16:28 +0800

    feat: paginate plugin

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,21 +1,31 @@
 <template>
   <nav>
-    <ul class="pagination">
-      <li v-for="n in pages"
-          class="page-item"
-          :key="`page-${n}`"
-          :class="{ 'active': n === page }">
-        <a class="page-link"
-           href=""
-           @click.prevent="$emit('update:page', n)">{{ n }}</a>
-      </li>
-    </ul>
+    <no-ssr>
+      <paginate
+        :page-count="pages"
+        :page-range="5"
+        :container-class="'pagination'"
+        :page-class="'page-item'"
+        :page-link-class="'page-link'"
+        :click-handler="handleClick"
+        :prev-text="'prev'"
+        :next-text="'next'"
+        :prev-class="'page-item'"
+        :next-class="'page-item'"
+        :prev-link-class="'page-link'"
+        :next-link-class="'page-link'"
+      >
+      </paginate>
+    </no-ssr>
   </nav>
 </template>
 
 <script>
+const Paginate = process.client ? require('vuejs-paginate') : undefined
+
 export default {
   name: 'Pagination',
+  components: { Paginate },
   props: {
     total: {
       type: Number,
@@ -30,11 +40,19 @@ export default {
       default: 10
     }
   },
+  data() {
+    return {
+    }
+  },
   computed: {
-    pages () {
-      return Math.ceil(this.total / this.perPage)
+    pages() {
+      return Math.ceil(this.total / this.perPage);
+    }
+  },
+  methods: {
+    handleClick(page) {
+      this.$emit('update:page', page);
     }
   }
 }
 </script>
-
